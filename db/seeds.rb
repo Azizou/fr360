99cbb0da99c3d03cb631f14a3ae0b767634c80d2
Azizou <azizou.android@gmail.com>
2014-12-03 14:25:05 +0200

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,15 +15,16 @@
                              ])
 
 User.create([
-                        {first_name: 'Azizou', last_name: 'Ogbone', email: 'ogboneazizou@gmail.com', password: 'test'},
-                        {first_name: 'Claude', last_name: 'Piessou', email: 'pcladius1@gmail.com', password: 'test'},
-                        {first_name: 'Ayaovi', last_name: 'Djissenou', email: 'djays@gmail.com', password: 'test'}
+                        {first_name: 'Azizou', last_name: 'Ogbone', email: 'ogboneazizou@gmail.com', password: 'testing123'},
+                        {first_name: 'Claude', last_name: 'Piessou', email: 'pcladius1@gmail.com', password: 'testing123'},
+                        {first_name: 'Ayaovi', last_name: 'Djissenou', email: 'djays@gmail.com', password: 'testing123'}
                     ])
 
 Navigation.create([{title: 'Home', position: 1, permalink: '/home'},
-                         { title: 'Users', position: 2, permalink: '/users', admin: true},
-                         { title: 'Questions', position: 3, permalink: '/questions', admin:true},
-                         { title: 'Dashboard', position: 1, permalink: '/admin/index', admin:true}
-                         ])
+                   { title: 'Users', position: 2, permalink: '/users', admin: true},
+                   { title: 'Users', position: 2, permalink: '/users', admin: true},
+                   { title: 'Questions', position: 3, permalink: '/questions', admin:true},
+                   { title: 'Dashboard', position: 1, permalink: '/admin/index', admin:true}
+                   ])
 
 Admin.create(first_name: 'Azizou', last_name: 'Ogbone', email: 'admin@test.com', password: 'test')
